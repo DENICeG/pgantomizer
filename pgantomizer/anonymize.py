ee07a860b64c5617719924ccb171cda48bb24bd9
root <root@fa1f96f8-b9be-40e9-a587-08425f01ff75.europe-west3-a.c.axial-device-220312.internal>
2019-01-24 09:35:45 +0000

    added cli parameter -t/--tableschema for processing non public schemas and tables set varchar column size dynamically bugfix ignoring primary key columns with ~

--- pgantomizer/anonymize.py
+++ pgantomizer/anonymize.py
@@ -67,22 +67,23 @@ def get_psql_db_args(db_args):
     return '-d {dbname} -U {user} -h {host} -p {port}'.format(**db_args)
 
 
-def drop_schema(db_args):
-    subprocess.run(
-        'PGPASSWORD={password} psql {db_args} -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" {redirect}'.format(
+def drop_schema(db_args,tableschema):
+    subprocess.call(
+        'PGPASSWORD={password} psql {db_args} -c "DROP SCHEMA {tableschema} CASCADE; CREATE SCHEMA {tableschema};" {redirect}'.format(
             password=db_args.get('password'),
             db_args=get_psql_db_args(db_args),
+            tableschema=tableschema,
             redirect='' if logging.getLogger().getEffectiveLevel() == logging.DEBUG else '>/dev/null 2>&1'),
         shell=True
     )
 
 
-def load_db_to_new_instance(filename, db_args):
+def load_db_to_new_instance(filename, db_args, tableschema):
     if not os.path.isfile(filename):
         raise IOError('Dump file {} is not a file.'.format(filename))
     os.putenv('PGPASSWORD', db_args.get('password'))
-    drop_schema(db_args)
-    subprocess.run(
+    drop_schema(db_args,tableschema)
+    subprocess.call(
         'PGPASSWORD={password} pg_restore -Fc -j 8 {db_args} {filename} {redirect}'.format(
             password=db_args.get('password'),
             db_args=get_psql_db_args(db_args), filename=filename,
@@ -91,16 +92,17 @@ def load_db_to_new_instance(filename, db_args):
     )
 
 
-def prepare_column_for_anonymization(conn, cursor, table, column, data_type):
+def prepare_column_for_anonymization(conn, cursor, table, column, data_type, character_maximum_length):
     """
     Some data types such as VARCHAR are anonymized in such a manner that the anonymized value can be longer that
     the length constrain on the column. Therefore, the constraint is enlarged.
     """
     if data_type == 'character varying':
         logging.debug('Extending length of varchar {}.{}'.format(table, column))
-        cursor.execute("ALTER TABLE {table} ALTER COLUMN {column} TYPE varchar(250);".format(
+        cursor.execute("ALTER TABLE {table} ALTER COLUMN {column} TYPE varchar({length});".format(
             table=table,
-            column=column
+            column=column,
+            length=character_maximum_length if character_maximum_length and character_maximum_length > 250 else 250
         ))
     conn.commit()
 
@@ -116,13 +118,14 @@ def check_schema(cursor, schema, db_args):
         raw_columns = schema[table].get('raw', [])
         custom_rule_columns = list(schema[table].get('custom_rules', {}).keys())
         columns_to_validate = raw_columns + custom_rule_columns
-        if pk_column is not None:
+        if pk_column is not None and pk_column != "~":
             columns_to_validate.append(pk_column)
         try:
-            cursor.execute("SELECT {columns} FROM {table} LIMIT 1;".format(
-                columns='"{}"'.format('", "'.join(columns_to_validate)),
-                table=table
-            ))
+            if columns_to_validate:
+               cursor.execute("SELECT {columns} FROM {table} LIMIT 1;".format(
+                   columns='"{}"'.format('", "'.join(columns_to_validate)),
+                   table=table
+               ))
         except psycopg2.ProgrammingError as e:
             raise InvalidAnonymizationSchemaError(str(e))
 
@@ -153,25 +156,30 @@ def get_column_update(schema, table, column, data_type):
         raise MissingAnonymizationRuleError('No rule to anonymize type "{}" for column "{}"'.format(data_type, column))
 
 
-def anonymize_table(conn, cursor, schema, table, disable_schema_changes):
+def anonymize_table(conn, cursor, schema, table, disable_schema_changes, tableschema):
 
-    logging.debug('Processing "{}" table'.format(table))
+    if tableschema == 'public':
+       tablewithschema = table
+    else:
+       tablewithschema = tableschema+"."+table
+
+    logging.debug('Processing "{}" table'.format(tablewithschema))
 
     # Truncate and return if desired
-    if schema[table] and schema[table].get('truncate', False) == True:
+    if schema[tablewithschema] and schema[tablewithschema].get('truncate', False) == True:
         logging.debug('Running TRUNCATE on {} ...'.format(table))
         cursor.execute('TRUNCATE {}'.format(table))
         return
 
     # Generate list of column_update SQL snippets for UPDATE
-    cursor.execute("SELECT column_name, data_type FROM information_schema.columns "
-        "WHERE table_schema = 'public' AND table_name = '{}'".format(table))
+    cursor.execute("SELECT column_name, data_type, character_maximum_length FROM information_schema.columns "
+        "WHERE table_schema = '{}' AND table_name = '{}'".format(tableschema,table))
     column_updates = []
     updated_column_names = []
-    for column_name, data_type in cursor.fetchall():
+    for column_name, data_type, character_maximum_length in cursor.fetchall():
         if not disable_schema_changes: # Bypass schema changes if explicitly requested
-            prepare_column_for_anonymization(conn, cursor, table, column_name, data_type)
-        column_update = get_column_update(schema, table, column_name, data_type)
+            prepare_column_for_anonymization(conn, cursor, tablewithschema, column_name, data_type, character_maximum_length)
+        column_update = get_column_update(schema, tablewithschema, column_name, data_type)
         if column_update is not None:
             column_updates.append(column_update)
             updated_column_names.append(column_name)
@@ -179,43 +187,45 @@ def anonymize_table(conn, cursor, schema, table, disable_schema_changes):
     # Process UPDATE if any column_updates requested
     if len(column_updates) > 0:
         update_statement = "UPDATE {table} SET {column_updates_sql} {where_clause}".format(
-            table=table,
+            table=tablewithschema,
             column_updates_sql=", ".join(column_updates),
-            where_clause="WHERE {}".format(schema[table].get('where', 'TRUE') if schema[table] else 'TRUE')
+            where_clause="WHERE {}".format(schema[tablewithschema].get('where', 'TRUE') if schema[tablewithschema] else 'TRUE')
         )
-        logging.debug('Running UPDATE on {} for columns {} ...'.format(table, ", ".join(updated_column_names)))
+        logging.debug('Running UPDATE on {} for columns {} ...'.format(tablewithschema, ", ".join(updated_column_names)))
         cursor.execute(update_statement)
     else:
         logging.debug('Nothing to anonymize for {}'.format(table))
 
 
-def anonymize_db(schema, db_args, disable_schema_changes):
+def anonymize_db(schema, db_args, disable_schema_changes,tableschema):
     with psycopg2.connect(**db_args) as conn:
         with conn.cursor() as cursor:
             check_schema(cursor, schema, db_args)
-            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type <> 'VIEW' ORDER BY table_name;")
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = '{tableschema}' AND table_type <> 'VIEW' ORDER BY table_name;".format(
+                tableschema=tableschema
+            ))
             for table_name in cursor.fetchall():
-                anonymize_table(conn, cursor, schema, table_name[0], disable_schema_changes)
+                anonymize_table(conn, cursor, schema, table_name[0], disable_schema_changes,tableschema)
             logging.debug('Anonymization complete!')
 
 
-def load_anonymize_remove(dump_file, schema, skip_restore=False, disable_schema_changes=False, leave_dump=False, db_args=None):
+def load_anonymize_remove(dump_file, schema, tableschema, skip_restore=False, disable_schema_changes=False, leave_dump=False, db_args=None):
     schema = yaml.load(open(schema))
     db_args = db_args or get_db_args_from_env()
 
     if skip_restore:
         logging.debug('Skipping restore process and using existing schema')
-        anonymize_db(schema, db_args, disable_schema_changes)
+        anonymize_db(schema, db_args, disable_schema_changes, tableschema)
     else:
         try:
-            load_db_to_new_instance(dump_file, db_args)
-            anonymize_db(schema, db_args, disable_schema_changes)
+            load_db_to_new_instance(dump_file, db_args,tableschema)
+            anonymize_db(schema, db_args, disable_schema_changes, tableschema)
         except Exception: # Any exception must result into droping the schema to prevent sensitive data leakage
-            drop_schema(db_args)
+            drop_schema(db_args,tableschema)
             raise
         finally:
             if not leave_dump:
-                subprocess.run(['rm', dump_file])
+                subprocess.call(['rm', dump_file])
 
 
 def main():
@@ -228,6 +238,7 @@ def main():
     parser.add_argument('-s', '--skip-restore', action='store_true', help='skips the restore process entirely, relying on existing DB')
     parser.add_argument('-d', '--disable-schema-changes', action='store_true', help='bypasses any column preparation that would affect schema definition')
     parser.add_argument('-l', '--leave-dump', action='store_true', help='do not delete dump file after anonymization')
+    parser.add_argument('-t', '--tableschema', help='name of the table schema', default='public' )
     parser.add_argument('--schema',  help='YAML config file with anonymization rules for all tables', required=True,
                         default='./schema.yaml')
     parser.add_argument('-f', '--dump-file',  help='path to the dump of DB to load and anonymize',
@@ -251,11 +262,10 @@ def main():
     if not os.path.isfile(args.schema):
         sys.exit('File with schema "{}" does not exist.'.format(args.schema))
 
-    db_args = ({name: value for name, value in zip(DB_ARG_NAMES, (args.dbname, args.user, args.password, args.host,
-                                                                  args.port))}
+    db_args = ({name: value for name, value in zip(DB_ARG_NAMES, (args.dbname, args.user, args.password, args.host, args.port))}
                if args.dbname and args.user else None)
 
-    load_anonymize_remove(args.dump_file, args.schema, args.skip_restore, args.disable_schema_changes, args.leave_dump, db_args)
+    load_anonymize_remove(args.dump_file, args.schema, args.tableschema, args.skip_restore, args.disable_schema_changes, args.leave_dump, db_args)
 
 
 if __name__ == '__main__':
